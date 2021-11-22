11f83d7a9d19f27018a993bedab9dc2dd1164287
Tchilo <tchiloross@gmail.com>
2021-11-22 11:11:43 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-const App = () => (<Router>
-    <Navbar/>
+const App = () => (
+  <Router>
+    <Navbar />
     <Routes>
-        <Route path="/" element={<Hompage />} />
-                </Routes>
-</Router>);
+      <Route path="/" element={<Hompage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
-// Add reducer/s here 
+// Add reducer/s here
 
 const reducer = combineReducers({ reducerName, reducerName });
 
